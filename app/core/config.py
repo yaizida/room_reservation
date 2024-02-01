f503d19e2324d6345b3cfe882ebe92d2e8bc02db
Никита Бурковский <myizizda@gmail.com>
2024-02-01 11:26:25 +0300

    create enigne & sessionmaker

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     app_title: str = 'Бронирование переговорок'
     app_description: str = 'Этот серивис позволит забронировать переговорку'
+    database_url: str
 
     class Config:
         env_file = '.env'
